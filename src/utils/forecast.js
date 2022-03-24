e0d5def3e4402393bec3e1adab34df1d582b50d1
Kalmanovich, Shai <Shai.Kalmanovich@verint.com>
2022-03-24 15:41:31 +0200

    Added humidity to the forecast API result

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitutde, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degress out')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degress out, and the humidity is: ' + body.current.humidity)
         }
     })
 }
